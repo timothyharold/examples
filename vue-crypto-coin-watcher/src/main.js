3b85468c74b7bc0f0468d28b79f2124b554b89de
timothyharold <5509877+timothyharold@users.noreply.github.com>
2018-05-16 17:46:45 -0500

    Update main.js

--- vue-crypto-coin-watcher/src/main.js
+++ vue-crypto-coin-watcher/src/main.js
@@ -6,9 +6,6 @@ import moment from 'moment'
 import ProductsStore from './modules/products-store'
 import Vuetify from 'vuetify'
 
-// import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
-// import 'material-design-icons-iconfont/dist/material-design-icons.css'
-
 Vue.use(Vuetify)
 Vue.use(Vuex)
 
